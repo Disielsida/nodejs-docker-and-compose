b0d76308c291896350b54104d4199d276aaac742
Andrei Sidorchenko <andrsidorchenko@gmail.com>
2025-12-07 12:21:22 +0300

    feat(cors): add domain to cors acception

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,9 +14,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000',
       'http://localhost:3001',
-      'http://localhost:5173',
+      'https://podaridaydisielsida.nomorepartiessbs.ru',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
