71a333b710b6e7827c64eb63ab3a509e04630f44
Andrei Sidorchenko <andrsidorchenko@gmail.com>
2025-12-07 15:34:18 +0300

    refactor(cors): update backend domen address

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3001',
-      'https://podaridaydisielsida.nomorepartiessbs.ru',
+      'https://api.podaridaydisielsida.nomorepartiessbs.ru',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
